5b7a1dababe2490a5f66602eecfb1b298a2cb249
Erik Drolshammer <erik-dev@fjas.no>
2015-11-17 22:15:47 +0100

    #12 - return 404 not found on /. Might consider a redirect later...?

--- src/main/java/net/whydah/identity/security/SecurityFilter.java
+++ src/main/java/net/whydah/identity/security/SecurityFilter.java
@@ -68,6 +68,11 @@ public class SecurityFilter implements Filter {
 
         //Require authenticated and authorized applicationtokenid
         String pathElement1 = findPathElement(pathInfo, 1);
+        //match /
+        if (pathElement1 == null) {
+            return HttpServletResponse.SC_NOT_FOUND;
+        }
+
         /*
         //match /password/{applicationtokenid}
         if (pathElement1.startsWith("/password")) {
