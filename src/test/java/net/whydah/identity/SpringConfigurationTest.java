4205cc67971a67211a2b73a4fe49fe5e785e315d
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-08-10 12:17:51 +0200

    Restore test. Needed database init.

--- src/test/java/net/whydah/identity/SpringConfigurationTest.java
+++ src/test/java/net/whydah/identity/SpringConfigurationTest.java
@@ -1,5 +1,10 @@
 package net.whydah.identity;
 
+import net.whydah.identity.dataimport.DatabaseMigrationHelper;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.constretto.ConstrettoBuilder;
+import org.constretto.ConstrettoConfiguration;
+import org.constretto.model.Resource;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,9 +14,34 @@ public class SpringConfigurationTest {
 
     @Test
     public void verifyConfigurationTest() {
+        ConstrettoConfiguration configuration = new ConstrettoBuilder()
+                .createPropertiesStore()
+                .addResource(Resource.create("classpath:useridentitybackend.properties"))
+                .addResource(Resource.create("file:./useridentitybackend_override.properties"))
+                .done()
+                .getConfiguration();
+        BasicDataSource dataSource = initBasicDataSource(configuration);
+        DatabaseMigrationHelper dbHelper = new DatabaseMigrationHelper(dataSource);
+        dbHelper.cleanDatabase();
+        dbHelper.upgradeDatabase();
+
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("context.xml");
         assertNotNull("lucene userdirectory is not null", classPathXmlApplicationContext.getBean("luceneUserDirectory"));
         assertNotNull("lucene applicationdirectory is not null", classPathXmlApplicationContext.getBean("luceneApplicationDirectory"));
         assertNotNull(classPathXmlApplicationContext.getBean("objectMapper"));
+    }
+
+    private static BasicDataSource initBasicDataSource(ConstrettoConfiguration configuration) {
+        String jdbcdriver = configuration.evaluateToString("roledb.jdbc.driver");
+        String jdbcurl = configuration.evaluateToString("roledb.jdbc.url");
+        String roledbuser = configuration.evaluateToString("roledb.jdbc.user");
+        String roledbpasswd = configuration.evaluateToString("roledb.jdbc.password");
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(jdbcdriver);
+        dataSource.setUrl(jdbcurl);
+        dataSource.setUsername(roledbuser);
+        dataSource.setPassword(roledbpasswd);
+        return dataSource;
     }
 }
